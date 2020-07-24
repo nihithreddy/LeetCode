e92da73749f0cabb970a9a36f20d22c52a644040
NihithReddy <sainihith9618@gmail.com>
2020-07-24 22:54:41 +0530

    committed

--- All Paths From Source to Target.cpp
+++ All Paths From Source to Target.cpp
@@ -2,28 +2,22 @@
 using namespace std;
 class Solution {
 public:
-    vector<vector<int>> paths;
-    vector<int> path;
-    void findPaths(vector<vector<int>> graph,vector<bool> vis,int cur,int n){
-        if(cur==n-1){
+    void findPaths(vector<vector<int>> graph,vector<vector<int>>& paths,vector<int> &path,int cur){
+        path.push_back(cur);
+        if(cur == (int)graph.size()-1){
             paths.push_back(path);
-            return;
         }
-        for(int v:graph[cur]){
-            if(vis[v]) continue;
-            path.push_back(v);
-            vis[v] = true;
-            findPaths(graph,vis,v,n);
-            path.pop_back();
-            vis[v] = !vis[v];
+        else{
+        for(auto v:graph[cur]){
+            findPaths(graph,paths,path,v);
         }
+        }
+        path.pop_back();  
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int nodes = graph.size();
-        vector<bool> vis(nodes,false);
-        path.push_back(0);
-        vis[0] = true;
-        findPaths(graph,vis,0,nodes);
+         vector<vector<int>> paths;
+         vector<int> path;
+        findPaths(graph,paths,path,0);
         return paths;
     }
 };
